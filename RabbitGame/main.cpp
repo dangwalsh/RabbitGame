12362c31857a67dc2a42d9339338fb3788ff318f
Daniel Walsh <dangwalsh@gmail.com>
2012-12-01 17:35:21 -0500

    Initial Commit

---
+++ RabbitGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RabbitGame
+//
+//  Created by Daniel Walsh on 12/1/12.
+//  Copyright (c) 2012 Daniel Walsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
